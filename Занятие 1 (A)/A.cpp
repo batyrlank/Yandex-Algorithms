67e0d706681e5d4267ed0e32699f2ef94145e04b
batyrlank <75443631+batyrlank@users.noreply.github.com>
2022-07-22 20:27:52 +0600

    Update A.cpp

--- Занятие 1 (A)/A.cpp
+++ Занятие 1 (A)/A.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+	// ( ax + b ) : ( cx + d ) = 0 -> ax + b = 0 and cx + d != 0
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if(a == 0 && b == 0){
+		cout << "INF\n";
+	}
+	else if(a == 0 || b * c == a * d){ // a / b = c / d -> a * d = b * c (по свойству пропорции)
+		cout << "NO\n";
+	}
+	else if(b % a == 0){
+		cout << (-1 * b) / a;
+	}
+	else{
+		cout << "NO\n";
+	}
+	return 0;
+}
